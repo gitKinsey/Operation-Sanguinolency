bf0a38f777543963ca51bdd62a27acedaef6c26a
gitKinsey <kingdom85@bluewin.ch>
2023-03-29 08:40:56 +0200

    improved brain fuck

--- Operation-Sanguinolency/Assets/Scripts/RotateAndDrift.cs
+++ Operation-Sanguinolency/Assets/Scripts/RotateAndDrift.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateObject : MonoBehaviour
+public class RotateAndDrift : MonoBehaviour
 {
     public float rotationSpeed = 50f;
+    public float driftSpeed = 5f;
+    public float lockedZPosition = 0f;
 
     void Update()
     {
@@ -12,5 +14,7 @@ public class RotateObject : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.Rotate(verticalInput * rotationSpeed * Time.deltaTime, 0, -horizontalInput * rotationSpeed * Time.deltaTime);
+        transform.position += new Vector3(horizontalInput * driftSpeed * Time.deltaTime, verticalInput * driftSpeed * Time.deltaTime, 0);
+        transform.position = new Vector3(transform.position.x, transform.position.y, lockedZPosition);
     }
 }
